e73df7f3f7b591fef9bbe9fe6003dc1473d13e10
VICTOR-81 <v161181@gmail.com>
2022-04-24 12:55:39 +0700

    continue

--- src/modules/menu-mobile/mobile_menu-header/mobile_menu-burger/MobileMenuBurger.jsx
+++ src/modules/menu-mobile/mobile_menu-header/mobile_menu-burger/MobileMenuBurger.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState, useContext } from 'react';
 import './mobile_menu-burger.scss'
 
 function MobileMenuBurger() {
-
+  // const [menuState, setMenuState] = useState(false);
   return(
 
+    // <div className="mobilemenu__burger" onClick={() => setMenuState(!menuState)}>
     <div className="mobilemenu__burger">
       <div className="mobilemenu__burger-item animation__burger"></div>
       <div className="mobilemenu__burger-item animation__burger"></div>
