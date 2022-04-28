17221960671cdce4eacca604ec8ed95751434199
VICTOR-81 <v161181@gmail.com>
2022-04-28 20:13:47 +0700

    continue

--- src/modules/pages/events_page/EventsPage.jsx
+++ src/modules/pages/events_page/EventsPage.jsx
@@ -42,7 +42,7 @@ function EventsPage(params) {
                 <img src={minus} alt="" />
               </div>
             </div>
-            <div className="events__content-items__wrapper">
+            <div className="events__content-items__wrapper active">
               <div className="events__content-items__left">
                   <div className="events__content-items__img">
                     <img src={eventcard1} alt="" />
@@ -63,14 +63,15 @@ function EventsPage(params) {
                       <span>Москва</span>
                     </div>
                     <div className="events__content-items__link">
-                      <a href="">Перейти на сайт</a>
+                      <a href="https://www.elle.ru/stil-zhizni/events/shakhmaty-eto-modno-gosti-brancha-chess-and-jazz-v-dukhe-italyanskoi-rivery/">Перейти на сайт</a>
                     </div>
                   </div>
                   <div className="events__content-items__text">
-                  Традиционный Brunch, который состоялся 22 августа 2021 в одном из самых красивых мест столицы — Императорском яхт клубе. Один незабываемый день в атмосфере итальянской ривьеры. Начиная с 15:00 играем в шахматы на набережной Москвы реки, танцуем и готовимся к фестивалю Chess & Jazz 2021 под алматинский джаз M’Dee и звуки трубы от Антохи MC, слушаем песни морю Тоси Чайкиной и двигаемся в такт с Untone Chernov и Oudima.
+                  Chess & Jazz Brunch состоялся 22 августа в одном из самых красивых мест столицы - Императорском яхт клубе. На один день гости события перенеслись в атмосферу итальянской ривьеры. Мы танцевали под алматинский джаз M’Dee и звуки трубы от Антохи MC, слушали песни морю Тоси Чайкиной и двигались в такт с Untone Chernov и Oudima. Чемпион Европы Эрнесто Инаркиев провел сеанс одновременной игры в шахматы, а затем сыграли блиц-турнир. Получился прекрасный день!
                   </div>
               </div>
             </div>
+//////////////////////////////////////////////////////////////////////////////////////////////////            
             <div className="events__content-items__item">
               <div>Chess & Jazz Brunch 2020</div>
               <div>
@@ -78,20 +79,147 @@ function EventsPage(params) {
                 <img src={minus} alt="" />
               </div>
             </div>
+            <div className="events__content-items__wrapper">
+              <div className="events__content-items__left">
+                  <div className="events__content-items__img">
+                    <img src={eventcard1} alt="" />
+                    <div className="events__content-items__btn">
+                      <button>
+                        <img src={left_arrow} alt="" />
+                      </button>
+                      <button>
+                        <img src={right_arrow} alt="" />
+                      </button>
+                    </div>
+                  </div>
+              </div>
+              <div className="events__content-items__right">
+                  <div className="events__content-items__top">
+                    <div className="events__content-items__date">
+                      <span>13 сентября 2020</span>
+                      <span>Москва</span>
+                    </div>
+                    <div className="events__content-items__link">
+                      <a href="https://www.buro247.ru/news/lifestyle/14-sep-2020-royal-studio-brunch-chess-jaz.html?gallery=180669&slide=5">Перейти на сайт</a>
+                    </div>
+                  </div>
+                  <div className="events__content-items__text">
+                  Первый Chess & Jazz Brunch стартовал 13 сентября 2020 года в атмосферном дворе пространства Royal Studio на Малой Ордынке. Хедлайнером шахматно-джазовой встречи стал Леван Горозия, который представил новый альбом “Рассвет”. Дресс-код дня preppy style тщательно был соблюден всем гостями события. Танцевали и играли в шахматы почти что до самого утра.
+                  </div>
+              </div>
+            </div>
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////            
             <div className="events__content-items__item">
-              <div>Chess & Jazz Festival 2019</div>
+              <div>Партия IV</div>
               <div>
                 <img src={plus} alt="" />
                 <img src={minus} alt="" />
               </div>
             </div>
+            <div className="events__content-items__wrapper">
+              <div className="events__content-items__left">
+                  <div className="events__content-items__img">
+                    <img src={eventcard1} alt="" />
+                    <div className="events__content-items__btn">
+                      <button>
+                        <img src={left_arrow} alt="" />
+                      </button>
+                      <button>
+                        <img src={right_arrow} alt="" />
+                      </button>
+                    </div>
+                  </div>
+              </div>
+              <div className="events__content-items__right">
+                  <div className="events__content-items__top">
+                    <div className="events__content-items__date">
+                      <span>12 декабря 2019</span>
+                      <span>Москва</span>
+                    </div>
+                    <div className="events__content-items__link">
+                      <a href="https://www.buro247.ru/news/lifestyle/13-dec-2019-party-chess-and-jazz.html?gallery=180218&slide=1">Перейти на сайт</a>
+                    </div>
+                  </div>
+                  <div className="events__content-items__text">
+                  Первый Chess & Jazz Brunch стартовал 13 сентября 2020 года в атмосферном дворе пространства Royal Studio на Малой Ордынке. Хедлайнером шахматно-джазовой встречи стал Леван Горозия, который представил новый альбом “Рассвет”. Дресс-код дня preppy style тщательно был соблюден всем гостями события. Танцевали и играли в шахматы почти что до самого утра.
+                  </div>
+              </div>
+            </div>
+/////////////////////////////////////////////////////////////////////////////////////////////////            
             <div className="events__content-items__item">
-              <div>Chess & Jazz Festival Singapore</div>
+              <div>Партия III</div>
               <div>
                 <img src={plus} alt="" />
                 <img src={minus} alt="" />
               </div>
             </div>
+            <div className="events__content-items__wrapper active">
+              <div className="events__content-items__left">
+                  <div className="events__content-items__img">
+                    <img src={eventcard1} alt="" />
+                    <div className="events__content-items__btn">
+                      <button>
+                        <img src={left_arrow} alt="" />
+                      </button>
+                      <button>
+                        <img src={right_arrow} alt="" />
+                      </button>
+                    </div>
+                  </div>
+              </div>
+              <div className="events__content-items__right">
+                  <div className="events__content-items__top">
+                    <div className="events__content-items__date">
+                      <span>18 июля 2019</span>
+                      <span>Москва</span>
+                    </div>
+                    <div className="events__content-items__link">
+                      <a href="https://www.cosmo.ru/lifestyle/news/18-07-2019/shahmatnaya-vecherinka-partiya-lll-v-preddverie-festivalya-chess-and-jazz/">Перейти на сайт</a>
+                    </div>
+                  </div>
+                  <div className="events__content-items__text">
+                  Официальной пре-пати к фестивалю Chess & Jazz 2019 стала Партия III. Место действия - веранда и внутренний оазис ресторана Blanc в Хохловском переулке. В главных ролях - Zoloto и Gayana. 
+                  </div>
+              </div>
+            </div>
+
+            <div className="events__content-items__item">
+              <div>Партия II</div>
+              <div>
+                <img src={plus} alt="" />
+                <img src={minus} alt="" />
+              </div>
+            </div>
+            <div className="events__content-items__wrapper active">
+              <div className="events__content-items__left">
+                  <div className="events__content-items__img">
+                    <img src={eventcard1} alt="" />
+                    <div className="events__content-items__btn">
+                      <button>
+                        <img src={left_arrow} alt="" />
+                      </button>
+                      <button>
+                        <img src={right_arrow} alt="" />
+                      </button>
+                    </div>
+                  </div>
+              </div>
+              <div className="events__content-items__right">
+                  <div className="events__content-items__top">
+                    <div className="events__content-items__date">
+                      <span>12 декабря 2019</span>
+                      <span>Москва</span>
+                    </div>
+                    <div className="events__content-items__link">
+                      <a href="https://www.cosmo.ru/lifestyle/news/18-07-2019/shahmatnaya-vecherinka-partiya-lll-v-preddverie-festivalya-chess-and-jazz/">Перейти на сайт</a>
+                    </div>
+                  </div>
+                  <div className="events__content-items__text">
+                  Однажды вас спросят, что вы любите больше - играть в шахматы или ходить на концерты? И вы ответите - Партию, потому что она собирает душевных шахматных оппонентов и джазменов в одном месте. Партия II прошла при поддержке неаполитанского бренда ISAIA в Петровском Пассаже, который в этот вечер был закрыт специально для наших гостей. Сергей Карякин провел блиц турнир перед концертом очаровательной джазовой певицы из Италии Francesca Tandoi. Владелец бренда и член семьи ISAIA Джанлука Исайя с удовольствием поддержал концепцию шахматно-джазовых вечеринок и, приехав в Москву, пообщался со всеми гостями за коктейлем. 
+                  </div>
+              </div>
+            </div>
+
           </div>
         </div>
         <div className="chess-button2">
